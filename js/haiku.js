d50addf44deb0cf479ad82e69e63faffd29d2a0f
Michael Brooks and Riki Montgomery <riki@>
2017-10-12 13:39:12 -0700

    add 3 passing tests

--- js/haiku.js
+++ js/haiku.js
@@ -1,36 +1,47 @@
 // constructor
 export class Syllable {
   constructor(line1, line2, line3) {
-    this.array = [line1, line2, line3]
+    this.array = [line1, line2, line3];
   }
 
-  eRemoval(input){
-    let words = input.toLowerCase().split(" ");
-    words = words.map((word) => {
-      // checks to see if the last letter = e
-      if (word.charAt(word.length - 1) === 'e') {
-        //if ^ true then this goes from start to end and takes that chunk of the string
-        return word.substr(0, word.length - 1);
-      } else {
-        return word;
-      }
+  eRemoval(){
+    let haiku = "";
+    this.array.forEach((line) =>{
+      let words = line.replace(/[^\w\s]|_/, "").split(" ");
+      words = words.map((word) => {
+        if (word.charAt(word.length - 1) === 'e') {
+          return word.substr(0, word.length - 1);
+        } else {
+          return word;
+        }
+      });
+      haiku += words.join(" ");
     });
-    return words.join(" ");
+    return haiku;
   }
 
-  // counts the number of vowels in a string and returns an int
   vowelCount(){
     let result = [];
     this.array.forEach((line) => {
       result.push((line.match(/[aeiou]{1,2}/gi,'')).length);
-    })
+    });
     return result;
   }
 }
 
-
-
-
+// eRemoval(input){
+//   let words = input.split(" ");
+//   words = words.map((word) => {
+//     // checks to see if the last letter = e
+//     if (word.charAt(word.length - 1) === 'e') {
+//       //if ^ true then this goes from start to end and takes that chunk of the string
+//       return word.substr(0, word.length - 1);
+//     } else {
+//       return word;
+//     }
+//   });
+//   return words.join(" ");
+// }
 
 // Really cool RegEx for reference
 // function otherRemoval(input){
--- spec/haiku-spec.js
+++ spec/haiku-spec.js
@@ -1,8 +1,19 @@
 import { Syllable } from './../js/haiku.js';
 
 describe('Syllable', function() {
+  it('will check for a total of 3 inputs', function() {
+    let input = new Syllable("The red foxes", "Jump over the fence.", "Likes to eat apples!");
+    console.log(input);
+    expect(input).toEqual(input)
+  });
+
   it('will scan an input and return the vowels count', function() {
     let vowel = new Syllable("The red foxes", "Jump over the fence.", "Likes to eat apples!");
     expect(vowel.vowelCount()).toEqual([4, 6, 6])
+  });
+
+  it('will remove slient e\'s at the end of any words in our string input', function() {
+    let input = new Syllable("The red foxes", "Jump over the fence.", "Likes to eat apples!");
+    expect(input.eRemoval()).toEqual('Th red foxesJump over th fencLikes to eat apples')
   });
 });
