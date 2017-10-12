8cd9105be1135aaf1f6ecf9275ce2555812b0b66
Michael Brooks and Riki Montgomery <michael@>
2017-10-12 10:14:35 -0700

    add first passing test to check vowelCount function

--- js/haiku.js
+++ js/haiku.js
@@ -1,41 +1,38 @@
 // constructor
-export class Syllable(line1) {
-  constructor() {
-    this.line1 = line1;
+export class Syllable {
+  constructor(line1, line2, line3) {
+    this.array = [line1, line2, line3]
+  }
 
+  eRemoval(input){
+    let words = input.toLowerCase().split(" ");
+    words = words.map((word) => {
+      // checks to see if the last letter = e
+      if (word.charAt(word.length - 1) === 'e') {
+        //if ^ true then this goes from start to end and takes that chunk of the string
+        return word.substr(0, word.length - 1);
+      } else {
+        return word;
+      }
+    });
+    return words.join(" ");
+  }
+
+  // counts the number of vowels in a string and returns an int
+  vowelCount(){
+    let result = [];
+    this.array.forEach((line) => {
+      result.push((line.match(/[aeiou]{1,2}/gi,'')).length);
+    })
+    return result;
   }
 }
 
-// counts the number of vowels in a string and returns an int
-function vowelCount(input){
-  return (input.match(/[aeiou]{1,2}/gi,'')).length;
-};
 
-// splits at spaces and returns an array of words
-function inputArr(input){
-  return input.toLowerCase().split(" ");
-};
 
-function eRemoval(input){
-  let words = input.toLowerCase().split(" ");
 
-  words = words.map(function(word){
-  if (word.charAt(word.length - 1) === 'e') {
-    return word.substr(0, word.length - 1)
-    } else {
-    return word;
-    }
-  });
-  return words.join(" ");
-}
 
-function otherRemoval(input){
-  return input.replace(/(\w*)es?(\W)/ig, "$1$2");
-}
-
-function otherRemoval(input){
-  return input.replace(/(\w*)(?:e|es)(\W)/ig, "$1$2$3");
-}
-
-console.log(eRemoval("The red fox"))
-console.log(otherRemoval("The red fox"))
+// Really cool RegEx for reference
+// function otherRemoval(input){
+//   return input.replace(/(\w*)es?(\W)/ig, "$1$2");
+// }
--- spec/haiku-spec.js
+++ spec/haiku-spec.js
@@ -1,8 +1,8 @@
 import { Syllable } from './../js/haiku.js';
 
-describe('Calculator', function() {
-  it('will replace 3 with pong', function() {
-    var calc = new Calculator('Hot Pink')
-    expect(calc.pingPong(3)).toEqual([1, 2, 'ping'])
+describe('Syllable', function() {
+  it('will scan an input and return the vowels count', function() {
+    let vowel = new Syllable("The red foxes", "Jump over the fence.", "Likes to eat apples!");
+    expect(vowel.vowelCount()).toEqual([4, 6, 6])
   });
 });
